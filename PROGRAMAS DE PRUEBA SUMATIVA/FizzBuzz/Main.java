de56f1db07cf13f06493bb6a7c398f724d862680
Ksegoviad <kielsegovia16@gmail.com>
2024-06-07 10:03:56 -0500

    first commit

--- PROGRAMAS DE PRUEBA SUMATIVA/FizzBuzz/Main.java
+++ PROGRAMAS DE PRUEBA SUMATIVA/FizzBuzz/Main.java
@@ -11,11 +11,11 @@ public class Main {
          */
         for (int i = 1; i<=100;i++){
             if (i % 3 ==0 && i % 5 == 0){
-            System.out.println(i+" fizzbuzz");
+                System.out.println("fizzbuzz");
             } else if (i % 5 == 0) {
-                System.out.println(i+ " buzz");
+                System.out.println( "buzz");
             } else if (i % 3 ==0) {
-                System.out.println(i+" fizz");
+                System.out.println("fizz");
             }else{
                 System.out.println(i);
             }
